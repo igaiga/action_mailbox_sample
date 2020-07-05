8e90f3011e6fd48b1a93c7942e9876851b47eb3b
Kuniaki Igarashi <igaiga@gmail.com>
2020-07-05 10:22:23 +0900

    Update action mailbox test adopt text

--- test/mailboxes/comments_mailbox_test.rb
+++ test/mailboxes/comments_mailbox_test.rb
@@ -1,16 +1,19 @@
 require "test_helper"
 
 class CommentsMailboxTest < ActionMailbox::TestCase
-  test 'メールの本文が、対応するBoardのコメントとして正しく追加されること' do
-    assert_difference -> { boards(:sample).comments.count } do
+  test "メールの本文が、対応するBoardのコメントとして正しく追加されること" do
+    board = Board.create
+    user = User.create(email: "test@example.com")
+
+    assert_difference -> { board.comments.count } do
       receive_inbound_email_from_mail \
-        to: "#{boards(:sample).id}-comment@example.com",
-        from: users(:willnet).email,
-        subject: 'メールタイトル',
-        body: 'こんにちは!'
+        to: "#{board.id}-comment@example.com",
+        from: user.email,
+        subject: "メールタイトル",
+        body: "こんにちは!"
     end
-    comment = boards(:sample).comments.last
-    assert_equal users(:willnet), comment.creator
-    assert_equal 'こんにちは!', comment.body
+    comment = board.comments.last
+    assert_equal user, comment.creator
+    assert_equal "こんにちは!", comment.body
   end
 end
